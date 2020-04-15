4db6c8253f49e6f4b1c57c053f08675096b0e262
pramodya971224 <56503875+pramodya971224@users.noreply.github.com>
2020-04-16 01:27:31 +0530

    Implement insert doctor method

--- Doctor_Service/src/com/AdminDocService.java
+++ Doctor_Service/src/com/AdminDocService.java
@@ -27,5 +27,24 @@ AdminDoc docObj = new AdminDoc();
 	public String readItems() {
 		return docObj.readDoctors();
 	}
+	
+	@POST 
+	@Path("/") 
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED) 
+	@Produces(MediaType.TEXT_PLAIN) 
+	public String insertDoctor(@FormParam("doc_nic") String nic, 
+							@FormParam("doc_fname") String fname, 
+							@FormParam("doc_lname") String lname, 
+							@FormParam("doc_email") String email,
+							@FormParam("doc_gender") String gender,
+							@FormParam("liscen_no") String liscen,
+							@FormParam("specialization") String special,
+							@FormParam("phone") String phone,
+							@FormParam("doc_charge") String charge,
+							@FormParam("user_user_id") String userId) 
+	{  
+		String output = docObj.insertDoctor(nic, fname, lname, email, gender, liscen, special, phone, charge, userId); 
+		return output; 
+	}
 
 }
--- Doctor_Service/src/model/AdminDoc.java
+++ Doctor_Service/src/model/AdminDoc.java
@@ -72,6 +72,49 @@ public class AdminDoc {
 		return output;
 	}
 	
+	public String insertDoctor(String nic, String fname, String lname, String email, String gender, String liscen,
+			String special, String phone, String charge, String userId) {
+		String output = "";
+
+		try (Connection con = DBConnector.getConnection()) {
+
+			if (con == null) {
+				return "Error while connecting to the database for inserting.";
+			}
+
+			// create a prepared statement
+			String query = " INSERT INTO doctor(`doc_id`, `doc_nic`, `doc_fname`, `doc_lname`, `doc_email`, `doc_gender`, `liscen_no`, `specialization`, `phone`, `doc_charge`, `user_user_id`)"
+					+ " VALUES (?,?,?,?,?,?,?,?,?,?,?)";
+
+			PreparedStatement preparedStmt = con.prepareStatement(query);
+
+			// binding values
+			preparedStmt.setInt(1, 0);
+			preparedStmt.setString(2, nic);
+			preparedStmt.setString(3, fname);
+			preparedStmt.setString(4, lname);
+			preparedStmt.setString(5, email);
+			preparedStmt.setString(6, gender);
+			preparedStmt.setString(7, liscen);
+			preparedStmt.setString(8, special);
+			preparedStmt.setInt(9, Integer.parseInt(phone));
+			preparedStmt.setDouble(10, Double.parseDouble(charge));
+			preparedStmt.setString(11, userId);
+
+			// execute the statement
+			preparedStmt.execute();
+			con.close();
+
+			output = "Inserted successfully";
+		} catch (Exception e) {
+			output = "Error while inserting the doctor.";
+			System.err.println(e.getMessage());
+		}
+
+		return output;
+	}
+
+	
 	
 
 }
