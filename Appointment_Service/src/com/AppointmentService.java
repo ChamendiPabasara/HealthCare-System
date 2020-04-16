429c8a6e8d9b8ae985d5c2e479b59d94b09411af
ChamendiPabasara <53684409+ChamendiPabasara@users.noreply.github.com>
2020-04-16 11:32:10 +0530

    Implementation of add Service

---
+++ Appointment_Service/src/com/AppointmentService.java
@@ -0,0 +1,51 @@
+package com;
+
+import java.sql.Date;
+
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+
+//For REST Service
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+//For JSON
+import com.google.gson.*;
+
+import model.Appointment;
+
+//For XML
+import org.jsoup.*;
+import org.jsoup.parser.*;
+import org.jsoup.nodes.Document;
+
+import model.Appointment;
+
+
+@Path("/Appointments")
+public class AppointmentService {
+	
+	Appointment App1 = new Appointment();
+
+	@GET
+	@Path("/")
+	@Produces(MediaType.TEXT_HTML)
+	public String ReadAppointments() {
+		
+		return App1.ReadAppointments();
+	}
+
+	@POST
+	@Path("/add")
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String addAppointment(@FormParam("date") Date day, @FormParam("time") String time,
+			@FormParam("patient_patient_id") int pid, 
+			@FormParam("doctor_doc_id") int did,
+			@FormParam("hospital_hosp_id") int hosID) 
+	{
+		String output = App1.addAppointment(day, time, pid, did, hosID);
+		return output;
+	}
+	
+
+}
