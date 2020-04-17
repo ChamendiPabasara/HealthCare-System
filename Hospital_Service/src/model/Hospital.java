ad87fdf406e729d62d08ff24523ea6738b214c52
Yasuni Chamodya <yasunichamodya98@gmail.com>
2020-04-17 11:32:22 +0530

    Implementation of update hospital method

--- Hospital_Service/src/model/Hospital.java
+++ Hospital_Service/src/model/Hospital.java
@@ -45,5 +45,39 @@ public class Hospital {
 		return output;
 	}
 
+	public String updateHospitals(String hID,String hname, String hadd, String hmail, String hphone, String hdate, String hchage) {
+		String output = "";
+		
+		try (Connection con = DBConnector.getConnection()){
+			if(con == null) {
+				return "Error while connecting to the database";
+			} else {
+				String query = "update hospital set hosp_name=?,hosp_address=?,hosp_email=?,hosp_phone=?,hosp_reg_date=?,hosp_charge=? where hosp_id=?";
+				PreparedStatement preparedStmt = con.prepareStatement(query);
+				
+				preparedStmt.setString(1, hname);
+				preparedStmt.setString(2, hadd);
+				preparedStmt.setString(3, hmail);
+				preparedStmt.setString(4, hphone);
+
+				java.util.Date date = new SimpleDateFormat("dd-MM-yyyy").parse(hdate);
+				java.sql.Date sqlDate = new java.sql.Date(date.getTime());
+
+				preparedStmt.setDate(5, sqlDate);
+				preparedStmt.setFloat(6, Float.parseFloat(hchage));
+				preparedStmt.setInt(7, Integer.parseInt(hID));
+				preparedStmt.execute();
+				con.close();
+				
+				output = "Updated Successfully";
+			}
+		} catch(Exception e) {
+			output = "Error while updating the hosital";
+			System.err.println(e.getMessage());
+		}
+		
+		return output;
+	}
+	
 	
 }
