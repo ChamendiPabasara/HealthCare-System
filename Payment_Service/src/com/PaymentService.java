9ebc67f0284540deeaa781f2867b248c7bd905d8
SanjalaSadini <sadini95kumarasiri@gmail.com>
2020-04-19 20:17:25 +0530

    Validate the Update Payment

--- Payment_Service/src/com/PaymentService.java
+++ Payment_Service/src/com/PaymentService.java
@@ -21,9 +21,9 @@ public class PaymentService {
 	@Path("/add")
 	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
 	@Produces(MediaType.TEXT_PLAIN)
-	public String addPayment(@NotEmpty(message = "Card type can't be empty") @FormParam("cardType") String cardType,
+	public String addPayment(@Pattern(regexp="^[a-z A-Z]*$", message="Can't use numbers on this feild") @NotEmpty(message = "Card type can't be empty") @FormParam("cardType") String cardType,
 			@NotNull(message = "Card Number can't be empty") @FormParam("cardNumber") int cardNumber,
-			@Pattern(regexp="^[a-z A-Z]*$", message="Can't use numbers on this feild") @NotNull(message = "Name on card can't be empty") @FormParam("nameOnCard") String nameOnCard,
+			@Pattern(regexp="^[a-z A-Z]*$", message="Can't use numbers on this feild") @NotEmpty(message = "Name on card can't be empty") @FormParam("nameOnCard") String nameOnCard,
 			@Digits(integer=3,fraction =0, message = "cvc can't be empty") @FormParam("cvc") int cvc,
 			@NotNull(message = "Expire date can't be empty") @FormParam("expireDate") Date expireDate,
 			@NotEmpty(message = "Status can't be empty") @FormParam("status") String status,
@@ -55,14 +55,14 @@ public class PaymentService {
 	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
 	@Produces(MediaType.TEXT_PLAIN)
 	public String updatePaymentById(@QueryParam("patientId") int id ,
-			@FormParam("cardType") String cardType,
-			@FormParam("cardNumber") int cardNumber,
-			@NotEmpty(message = "Name on card can't be empty") @FormParam("nameOnCard") String nameOnCard,
-			@FormParam("cvc") int cvc,
-			@FormParam("expireDate") Date expireDate,
-			@FormParam("status") String status,
-			@FormParam("paymentDate") Date paymentDate,
-			@FormParam("appointmentId") int appointmentId ) {
+			@Pattern(regexp="^[a-z A-Z]*$", message="Can't use numbers on this feild") @NotEmpty(message = "Card type can't be empty") @FormParam("cardType") String cardType,
+			@NotNull(message = "Card Number can't be empty") @FormParam("cardNumber") int cardNumber,
+			@Pattern(regexp="^[a-z A-Z]*$", message="Can't use numbers on this feild") @NotEmpty(message = "Name on card can't be empty") @FormParam("nameOnCard") String nameOnCard,
+			@Digits(integer=3,fraction =0, message = "cvc can't be empty") @FormParam("cvc") int cvc,
+			@NotNull(message = "Expire date can't be empty") @FormParam("expireDate") Date expireDate,
+			@NotEmpty(message = "Status can't be empty") @FormParam("status") String status,
+			@NotNull(message = "Payment Date can't be empty") @FormParam("paymentDate") Date paymentDate,
+			@Digits(integer=5,fraction =0,message = "Appointment can't be empty") @FormParam("appointmentId") int appointmentId ) {
 		
 	return this.payment.updatePayment(id,cardType,cardNumber,nameOnCard,cvc,expireDate,status,paymentDate,appointmentId);
 	}
